10ec69636a5b3e69bb554d81424430ad42b4670f
Matt Wizeman <matt.wizeman@gmail.com>
2019-05-14 13:41:38 -0400

    wip

--- inmemory/src/main/java/org/vertexium/inmemory/search/DefaultSearchIndex.java
+++ inmemory/src/main/java/org/vertexium/inmemory/search/DefaultSearchIndex.java
@@ -98,7 +98,7 @@ public class DefaultSearchIndex implements SearchIndex {
 
     @Override
     public org.vertexium.search.MultiVertexQuery queryGraph(Graph graph, String[] vertexIds, String queryString, User user) {
-        throw new VertexiumException("Not Yet Implemented");
+        return new DefaultMultiVertexQuery(graph, vertexIds, queryString, user);
     }
 
     @Override
